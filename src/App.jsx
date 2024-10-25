56af3ec7a56a70d9378b036941895d5faa7b60f6
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-10-25 14:57:47 +0700

    add: fetched the api with axios and render the list with map

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios"
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [input, setInput] = useState("");
+  const [data, setData] = useState([]);
+  
+
+  useEffect(() => {
+    result();
+  }, [input]);
+
+  const result = async () => {
+    try {
+      const data = await axios.get(
+        `https://openlibrary.org/search.json?q=${input}&limit=10`
+      );
+      setData(data.data.docs);
+      
+    } catch (error) {}
+  };
+
+  return (
+    <>
+      <div className="App">
+        <h1>Find a Book</h1>
+        <input
+          onChange={(event) => setInput(event.target.value)}
+        ></input>
+      </div>
+      <div>
+        <ul>{
+          data.map((item, index) => {
+            return <div key={index}>{item.title}</div>
+          })
+          }</ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
