cd1c659b84977afe5afcb5fb534b315e93ef9cf1
Werapol Jaranai <r5r5wa55@hotmail.com>
2024-10-25 14:58:07 +0700

    feat:search

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [searchBook, setSearchBook] = useState("react");
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  useEffect(() => {
+    resBook();
+  }, [searchBook]);
+
+  const resBook = async () => {
+    try {
+      const res = await axios.get(
+        `https://openlibrary.org/search.json?q=${searchBook}&limit=10`
+        // https://www.googleapis.com/books/v1/volumes?q=<query-param-value>
+      );
+      // console.log(res);
+
+      setData(res.data.docs);
+    } catch (error) {}
+  };
+
+  return (
+    <>
+      <div className="App">Find a Book</div>
+      <input type="text" onChange={(e) => setSearchBook(e.target.value)} />
+      <ul>
+        {data.map((data, index) => (
+          <li className="" key={index}>
+            {data.title}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
